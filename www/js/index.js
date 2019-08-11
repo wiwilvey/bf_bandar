a0e5f68b18bd0ea9c5db64abe8323b3351aa3346
wiwilvey <wilveywhye@gmail.com>
2019-08-11 19:22:29 +0700

    komit perdana

---
+++ www/js/index.js
@@ -0,0 +1,13 @@
+$(document).ready( function(){
+    $("#menuSPP").click( function(){
+        window.location='dataSpp.html';
+    })
+
+    $("#menuDU").click( function(){
+        window.location='dataDu.html';
+    })
+
+    $("#menuSpend").click( function(){
+        window.location='dataPengeluaran.html';
+    })
+})
